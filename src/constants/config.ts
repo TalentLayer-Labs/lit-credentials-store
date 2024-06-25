48617fdea6a3d5e4539a6a466c9507fbc0bdd4b2
Bertrand <bertrandrigal.pro@gmail.com>
2024-06-25 16:45:40 +0200

    Update lit client dep

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,5 +1,5 @@
 import { env } from "@/env.mjs";
 
-export const IS_USING_LIT_ACTION = true; // else = nextjs query
-export const FIXED_PKP = env.NEXT_PUBLIC_LIT_PKP || null; // set your pkp here or leave blank to let the user generate one by himself
+export const IS_USING_LIT_ACTION = false; // else = nextjs query
+export const FIXED_PKP = process.env.NEXT_PUBLIC_LIT_PKP || null; // set your pkp here or leave blank to let the user generate one by himself
 export const TEST_MODE = true;

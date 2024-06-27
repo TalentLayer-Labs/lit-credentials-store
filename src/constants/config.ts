6c8e7ae02a9514d8538552d72c4a6e82998a3bcf
Bertrand <bertrandrigal.pro@gmail.com>
2024-06-27 09:47:12 +0200

    Fix lit action condition

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,4 +1,4 @@
 import { env } from "@/env.mjs";
 
-export const IS_USING_LIT_ACTION = false; // else = nextjs query
+export const IS_USING_LIT_ACTION = true; // else = nextjs query
 export const FIXED_PKP = process.env.NEXT_PUBLIC_LIT_PKP || null; // set your pkp here or leave blank to let the user generate one by himself
